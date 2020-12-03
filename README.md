8de531bd9c9021ff2e9d143b234650947d2461c4
erzenH <erzen336@gmail.com>
2020-12-03 10:30:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ngrx-store-full-example-qkw3bq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ngrx-store-full-example-qkw3bq)
---
+++ src/app/devices/devices.module.ts
@@ -0,0 +1,61 @@
+import { NgModule } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+// import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { MatIconModule } from '@angular/material';
+
+import { SharedModule } from './../shared';
+
+import { DevicesRoutingModule } from './device-routing.module';
+
+import { DevicesOverviewComponent } from './devices-overview.component';
+import { DevicesComponent } from './devices.component';
+import { DeviceService } from './../services/device-mgr.service';
+import { MomentModule } from 'ngx-moment';
+import { DeviceEffects } from './device.effects';
+import {reducer} from './device.reducers';
+
+@NgModule({
+  imports: [
+    SharedModule,
+    MomentModule,
+
+    DevicesRoutingModule,
+    StoreModule.forFeature('devices', reducer),
+    TranslateModule.forChild({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      },
+      isolate: true
+    }),
+    EffectsModule.forFeature([
+      DeviceEffects
+    ]),
+
+    MatIconModule,
+
+  ],
+  declarations: [
+    DevicesComponent,
+    DevicesOverviewComponent,
+  ],
+  providers: [DeviceService]
+})
+export class DevicesModule {
+  constructor() { }
+}
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(
+    http,
+    `/assets/i18n/examples/`,
+    '.json'
+  );
+}
